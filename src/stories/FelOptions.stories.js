37bc6c960d3dc0195357791ee2d6f58ebeb67cb8
Steven Weaver <sweaver@temple.edu>
2025-02-12 14:48:48 -0800

    updating fel story

--- src/stories/FelOptions.stories.js
+++ src/stories/FelOptions.stories.js
@@ -17,24 +17,3 @@ Default.args = {
 		console.log("Running FEL Analysis with options:", options);
 	},
 };
-
-// Add additional stories to showcase different states if needed
-
-export const WithEmail = Template.bind({});
-WithEmail.args = {
-	runFEL: (options) => {
-		console.log("Running FEL Analysis with options:", options);
-	},
-};
-
-export const WithAdvancedOptions = Template.bind({});
-WithAdvancedOptions.args = {
-	runMethod: (options) => {
-		console.log("Running FEL Analysis with options:", options);
-		// Example values to demo advanced options
-		return {
-			resample: 100,
-			confidenceInterval: true,
-		};
-	},
-};
