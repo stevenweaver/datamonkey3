a1cdfcb1ef5d0afa222a10b9908cbcb131fb5d33
Steven Weaver <sweaver@temple.edu>
2025-02-26 22:10:20 -0800

    use environment variables for iframe config

--- src/lib/config/env.ts
+++ src/lib/config/env.ts
@@ -1,5 +1,7 @@
 // Environment configuration
-export const PAGES_URL = import.meta.env.VITE_PAGES_URL || '//localhost:3000/pages';
+export const PAGES_URL = import.meta.env.VITE_PAGES_URL ||
+  "//localhost:3000/pages";
 
-// When in production, this will be '//responsive-iframe.hyphy-eye.pages.dev/pages'
+// When in production (or cloudflare), this will be '//responsive-iframe.hyphy-eye.pages.dev/pages'
 // In development, it will be '//localhost:3000/pages'
+
